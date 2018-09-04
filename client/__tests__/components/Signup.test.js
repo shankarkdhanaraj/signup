0894f22ea60d01c350c0893020878afc9bc1c931
Shankar <shankarkdhanaraj@gmail.com>
2018-09-03 23:16:35 -0400

    setup Material UI email password fields for signup

--- client/__tests__/components/Signup.test.js
+++ client/__tests__/components/Signup.test.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import { shallow } from 'enzyme';
 import Signup from '../../src/components/Signup.jsx';
 import setupJestEnzyme from '../setup/setupJestEnzyme.js';
+import { Input, InputLabel, FormControl } from '@material-ui/core';
+import { createShallow } from '@material-ui/core/test-utils';
 
 describe('signup', () => {
   var signup;
+  // let shallow;
+
   beforeEach( () => {
+    // shallow = createShallow();
     signup = shallow(<Signup />);
   });
 
@@ -15,13 +20,20 @@ describe('signup', () => {
   });
 
   it('has a place to enter the first and last name', () => {
-    expect(signup.find('input.first-name')).toHaveLength(1);
-    expect(signup.find('input.last-name')).toHaveLength(1);
-    // expect(signup.find('input.first-name').length).toBe(1); - also works
+    expect(signup.find(Input).filter('.first-name')).toHaveLength(1);
+    expect(signup.find(Input).filter('.last-name')).toHaveLength(1);
   });
 
-  it('tells the user which field to enter the first and last name', () => {
-    expect(signup.find('label.first-name')).toHaveLength(1);
-    expect(signup.find('label.last-name')).toHaveLength(1);
+  it('indicates which field to enter the first and last name', () => {
+    expect(signup.find(InputLabel).filter('.first-name')).toHaveLength(1);
+    expect(signup.find(InputLabel).filter('.last-name')).toHaveLength(1);
+  });
+
+  it('has a place to enter the email address', () => {
+    expect(signup.find(Input).filter('.email-address')).toHaveLength(1);
+  });
+
+  it('has a place to enter the password', () => {
+    expect(signup.find(Input).filter('.password')).toHaveLength(1);
   });
 });
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,24 +1,68 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import {
+  Input,
+  InputLabel,
+  FormControl,
+  InputAdornment,
+} from '@material-ui/core';
+import Email from '@material-ui/icons/Email';
+import VpnKey from '@material-ui/icons/VpnKey';
 
 
 function Signup() {
   return (
     <div>
       <div>
-        <label htmlFor="signup-first-name" className="first-name">
-          First Name
-          <input type="text" className="first-name" id="signup-first-name" />
-        </label>
+        <FormControl required>
+          <InputLabel htmlFor="signup-first-name" className="first-name">First Name</InputLabel>
+          <Input type="text" id="signup-first-name" className="first-name" placeholder="First Name" />
+        </FormControl>
       </div>
       <div>
-        <label htmlFor="signup-last-name" className="last-name">
-          Last Name
-          <input type="text" className="last-name" id="signup-last-name" />
-        </label>
+        <FormControl required>
+          <InputLabel htmlFor="signup-last-name" className="last-name">Last Name</InputLabel>
+          <Input type="text" id="signup-last-name" className="last-name" placeholder="Last Name" />
+        </FormControl>
+      </div>
+      <div>
+        <FormControl required>
+          <InputLabel htmlFor="signup-email-address" className="email-address">Email</InputLabel>
+          <Input
+            type="email"
+            id="signup-email-address"
+            className="email-address"
+            placeholder="drone@globex.com"
+            startAdornment={
+              (
+                <InputAdornment position="start">
+                  <Email />
+                </InputAdornment>
+              )
+            }
+          />
+        </FormControl>
+      </div>
+      <div>
+        <FormControl required>
+          <InputLabel htmlFor="signup-password" className="password">Password</InputLabel>
+          <Input
+            type="email"
+            id="signup-password"
+            className="password"
+            startAdornment={
+              (
+                <InputAdornment position="start">
+                  <VpnKey />
+                </InputAdornment>
+              )
+            }
+          />
+        </FormControl>
       </div>
     </div>
   );
 }
+
 
 export default hot(module)(Signup);
